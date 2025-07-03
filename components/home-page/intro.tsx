200fdcfc529ca04b57d0943726b0893befcc1f36
diallee <diallee@126.com>
2025-07-03 15:28:24 +0800

    feat: fork同步修改和部分问题修复

--- components/home-page/intro.tsx
+++ components/home-page/intro.tsx
@@ -3,10 +3,11 @@ import { Twemoji } from '~/components/ui/twemoji'
 export function Intro() {
   return (
     <h1 className="text-neutral-900 dark:text-neutral-200">
-      我是 <span className="font-medium">dial lee</span> -一位来自于中国南京的软件工程师
+      我是 <span className="font-medium">dial lee</span>{' '}
+      -一位来自于中国南京的软件工程师
       <span className="hidden font-medium">Viet Nam</span>
       <span className="absolute ml-1.5 inline-flex pt-[3px]">
-        <Twemoji emoji="twa-flag-china" />
+        <Twemoji emoji="flag-china" />
       </span>
     </h1>
   )
